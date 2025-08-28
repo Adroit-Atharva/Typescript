802887b8d3e4886d28b69554ca724b37403b2db1
Atharva_Birjdar <birajdaratharva@gmail.com>
2025-08-28 16:30:56 +0530

    adding some typescript stuff

---
+++ typescript/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello World");
+let age = 12;
+if (age < 18)
+    age = age + 10;
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ typescript/src/index.ts
@@ -0,0 +1,8 @@
+console.log("Hello World");
+
+let age: number = 12;
+
+if(age<18)
+    age = age + 10;
+
+console.log(age);
